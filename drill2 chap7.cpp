14ae1daa3ab9f8eafefe97b7ae2b86cbe90a9fd7
Soukaina-Ahlach <99406964+Soukaina-Ahlach@users.noreply.github.com>
2022-05-10 23:23:21 +0200

    Update drill2 chap7.cpp

--- drill2 chap7.cpp
+++ drill2 chap7.cpp
@@ -1,298 +1,233 @@
+//p1
 #include "std_lib_facilities.h"
-// task 6
-#define k 1000
 
+struct Date //data type date
+{
+	Date(int y, int m, int d);
 
-struct Token {
-	char kind;
-	double value;
-	string name;
-	Token(char ch) :kind(ch), value(0) { }
-	Token(char ch, double val) :kind(ch), value(val) { }
-	// add this so it accept string too
-	Token(char ch, string S) :kind(ch), name(S) { }
+	int y;
+	int m;
+	int d;
 };
 
-class Token_stream {
-	bool full;
-	Token buffer;
-public:
-	Token_stream() :full(0), buffer(0) { }
 
-	Token get();
-	void unget(Token t) { buffer = t; full = true; }
-
-	void ignore(char);
-};
-
-const char let = '#';// changed from let to # task 10
-const char quit = 'Q';
-const char print = ';';
-const char number = '8';
-const char name = 'a';
-// added
-const char sqrtt = 's';
-const char poww = 'p';
-
-Token Token_stream::get()
+Date::Date(int yy, int mm, int dd) //constructor for initialisation
 {
-	if (full) { full = false; return buffer; }
-	char ch;
-	cin >> ch;
-	switch (ch) {
-	case '(':
-	case ')':
-	case '+':
-	case '-':
-	case '*':
-	case '/':
-	case '%':
-	case ';':
-	case '=':
-	case','://added this to be used in pow
-		return Token(ch);
-	case '.':
-	case '0':
-	case '1':
-	case '2':
-	case '3':
-	case '4':
-	case '5':
-	case '6':
-	case '7':
-	case '8':
-	case '9':
-	{	cin.unget();
-	double val;
-	cin >> val;
-	return Token(number, val);
-	}
-	default:
-		if (isalpha(ch)) {
-			string s;
-			s += ch;
-			while (cin.get(ch) && (isalpha(ch) || isdigit(ch)))
-				s += ch;//+= like one above
-			cin.unget();
-			if (s == "#") return Token(let);//task 10 too
-			if (s == "exit") return Token(quit);//task 11 , i didnt defined string,i just give it as "exit"
-			//added sqrt and power to be used as cases later
-			if (s == "sqrt") return Token(sqrtt);
-			if (s == "pow") return Token(poww);
-			return Token(name,s);
-		}
-		error("Bad token");
-	}
-}
-
-void Token_stream::ignore(char c)
-{
-	if (full && c == buffer.kind) {
-		full = false;
-		return;
-	}
-	full = false;
-
-	char ch;
-	while (cin >> ch)
-		if (ch == c) return;
-}
-
-struct Variable {
-	string name;
-	double value;
-	Variable(string n, double v) :name(n), value(v) { }
-};
-
-vector<Variable> names;
-
-double get_value(string s)
-{
-	for (int i = 0; i < names.size(); ++i)
-		if (names[i].name == s) return names[i].value;
-	error("get: undefined name ", s);
-}
-
-void set_value(string s, double d)
-{
-	for (int i = 0; i <= names.size(); ++i)
-		if (names[i].name == s) {
-			names[i].value = d;
-			return;
-		}
-	error("set: undefined name ", s);
-}
-
-bool is_declared(string s)
-{
-	for (int i = 0; i < names.size(); ++i)
-		if (names[i].name == s) return true;
-	return false;
-}
-
-Token_stream ts;
-
-double expression();
-
-double primary()
-{
-	Token t = ts.get();
-	switch (t.kind) {
-	case '(':
-	{	double d = expression();
-	t = ts.get();
-	if (t.kind != ')') error("')' expected");
-	}
-	case '-':
-		return -primary();
-	case number:
-		return t.value;
-	case name:
-		return get_value(t.name);
-		//sqrt task 7
-	case sqrtt:
+	int Minyear(1900), Maxyear(3000);
+	if (yy > Maxyear || yy < Minyear)
+		cout << " year is not in calender.";
+	if (mm > 12 || mm < 1)
+		cout << "month not in calender. ";
+	if (dd > 31 || dd < 1)
+		cout << "day not in  calender." << endl;
+	else
 	{
-		t = ts.get();
-		if (t.kind != '(')
-			error(" '(' was expected.");
-		double d = expression();
-		if (d < 0)
-			error("negative number");//task8
-		t = ts.get();
-		if (t.kind != ')')
-			error(" ')' was expected.");
-
-		return sqrt(d);
-	}
-	//task 9
-	case poww :
-	{
-		t = ts.get();//for (
-		if (t.kind != '(')
-			error(" '(' was expected.");
-		double d = expression();// for number
-		 t = ts.get();// for comma
-		 if (t.kind != ',')
-			 error(" ',' was expected.");
-		 double i = expression();// for i
-		 t = ts.get();//for )
-		 if (t.kind != ')')
-			 error(" ')' was expected.");
-
-		 return pow(d, i);
-	}
-	default:
-		error("primary expected");
+		y = yy; m = mm; d = dd;
 	}
 }
 
-double term()
+
+void add_day(Date& dd, int n)
 {
-	double left = primary();
-	while (true) {
-		Token t = ts.get();
-		switch (t.kind) {
-		case '*':
-			left *= primary();
-			break;
-		case '/':
-		{	double d = primary();
-		if (d == 0) error("divide by zero");
-		left /= d;
-		break;
-		}
-		default:
-			ts.unget(t);
-			return left;
-		}
-	}
+	//increase dd by n days
+	dd.d += n;
 }
 
-double expression()
+
+ostream& operator<<(ostream& os, const Date& d)
 {
-	double left = term();
-	while (true) {
-		Token t = ts.get();
-		switch (t.kind) {
-		case '+':
-			left += term();
-			break;
-		case '-':
-			left -= term();
-			break;
-		default:
-			ts.unget(t);
-			return left;
-		}
-	}
+	return os << "(" << d.y
+		<< "," << d.m
+		<< "," << d.d << ")";
 }
 
-double declaration()
-{
-	Token t = ts.get();
-	if (t.kind != 'a') error("name expected in declaration");
-	string name = t.name;
-	if (is_declared(name)) error(name, " declared twice");
-	Token t2 = ts.get();
-	if (t2.kind != '=') error("= missing in declaration of ", name);
-	double d = expression();
-	names.push_back(Variable(name, d));
-	return d;
-}
-
-double statement()
-{
-	Token t = ts.get();
-	switch (t.kind) {
-	case let:
-		return declaration();
-	default:
-		ts.unget(t);
-		return expression();
-	}
-}
-
-void clean_up_mess()
-{
-	ts.ignore(print);
-}
-
-const string prompt = "> ";
-const string result = "= ";
-
-void calculate()
-{
-	while (true) try {
-		cout << prompt;
-		Token t = ts.get();
-		while (t.kind == print) t = ts.get();
-		if (t.kind == quit) return;
-		ts.unget(t);
-		cout << result << statement() << endl;
-	}
-	catch (runtime_error& e) {
-		cerr << e.what() << endl;
-		clean_up_mess();
-	}
-}
 
 int main()
 {
-	try {
-		calculate();
-		return 0;
+	Date today( 1976, 8, 28 );
+	Date tomorrow( today );
+
+	add_day(tomorrow, 1);
+
+	cout << today << endl;
+	cout << tomorrow << endl;
+
+	//invalid date
+	Date invalidDate{ 1800, 27, 32 };
+
+	keep_window_open();
+	return 0;
+}
+-------------------------------------------------------------------------------------------------------------------------------------------
+//p3
+	
+	#include "std_lib_facilities.h"
+
+// -----------------------------------------------------------------------------
+
+class Date
+{
+public:
+	Date(int y, int m, int d);
+
+	void add_day(int n);
+
+	int month() const { return m; }
+	int day() const { return d; }
+	int year() const { return y; }
+
+private:
+	int y, m, d; //
+};
+
+// -----------------------------------------------------------------------------
+
+Date::Date(int yy, int mm, int dd)
+{
+	int yearMin(1900), yearMax(3000);
+	if (yy > yearMax || yy < yearMin)
+		cout << "year is not in calender.";
+	if (mm > 12 || mm < 1)
+		cout << "month is not in calender.";
+	if (dd > 31 || dd < 1)
+		cout << " day not in calender." << endl;
+	else
+	{
+		y = yy; m = mm; d = dd;
 	}
-	catch (exception& e) {
-		cerr << "exception: " << e.what() << endl;
-		char c;
-		while (cin >> c && c != ';');
-		return 1;
+}
+
+// -----------------------------------------------------------------------------
+
+void Date::add_day(int n)
+{
+	//if d += n is more than 31, make it 1, else return d
+	if (d += n > 31)
+	{
+		d = 1;
+		++m;
 	}
-	catch (...) {
-		cerr << "exception\n";
-		char c;
-		while (cin >> c && c != ';');
-		return 2;
+}
+
+// -----------------------------------------------------------------------------
+
+ostream& operator<<(ostream& os, const Date& d)
+{
+	return os << "(" << d.year()
+		<< "," << d.month()
+		<< "," << d.day() << ")";
+}
+
+// -----------------------------------------------------------------------------
+
+int main()
+{
+	Date today( 1978, 6, 31 );
+	Date tomorrow( today );
+
+	tomorrow.add_day(1);
+
+	cout << today << endl;
+	cout << tomorrow << endl;
+
+	//invalid date
+	Date invalidDate{ 1300, 27, -4 };
+
+	keep_window_open();
+	return 0;
+}
+------------------------------------------------------------------------------------------------------------------------------------
+	//p4
+	#include "std_lib_facilities.h"
+
+// -----------------------------------------------------------------------------
+
+enum class Month //defined an enumeration class
+{
+	jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec//created user defined data type month
+};
+
+// -----------------------------------------------------------------------------
+
+class Date
+{
+public:
+	Date(int y, Month m, int d);
+
+	void add_day(int n);
+
+	Month month() const { return m; }
+	int day() const { return d; }
+	int year() const { return y; }
+
+private:
+	int y, d;
+	Month m;
+};
+
+// -----------------------------------------------------------------------------
+
+Date::Date(int yy, Month mm, int dd)
+{
+	int yearMin(1900), yearMax(3000);
+	if (yy > yearMax || yy < yearMin)
+		cout << "year is not in calender.";
+	if (int(mm) > 12 || int(mm) < 1) // you get a c cast
+		cout << " month  is not in calender.";
+	if (dd > 31 || dd < 1)
+		cout << " day  is not in calender." << endl;
+	else
+	{
+		y = yy; m = mm; d = dd;
 	}
+}
+
+// -----------------------------------------------------------------------------
+
+void Date::add_day(int n)
+{
+	//if d += n is more than 31, make it 1, else return d
+	if (d += n > 31)
+	{
+		d = 1;
+		int nextM = int(m) + 1;		// and you get a c cast
+		if (nextM > 12)
+		{
+			m = Month::jan;
+			if (++y > 3000)
+				cout << "szia! it's not year 3000; " << endl;
+		}
+		else
+			m = Month(nextM);			//and you get a c cast
+	}
+}
+
+// -----------------------------------------------------------------------------
+
+ostream& operator<<(ostream& os, const Date& d)
+{
+	return os << "(" << d.year()
+		<< "," << int(d.month())	// you all get c casts!
+		<< "," << d.day() << ")";
+}
+
+// -----------------------------------------------------------------------------
+
+int main()
+{
+	//c casting everywhere!
+
+	Date today( 1978, Month::dec, 31 );
+	Date tomorrow( today );
+
+	tomorrow.add_day(1);
+
+	cout << today << endl;
+	cout << tomorrow << endl;
+
+	//invalid date
+	Date invalidDate{ 1300, Month::dec, -4 };
+
+	keep_window_open();
 	return 0;
 }
